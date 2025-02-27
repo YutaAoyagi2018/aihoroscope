942aa6db8b593c84a4feffba0a18b66cfd4e2249
Yuta Aoyagi <yuta_a2008@yahoo.co.jp>
2025-02-27 19:43:11 +0900

    ads.txtのurl設定追加

--- horoscope_app/urls.py
+++ horoscope_app/urls.py
@@ -1,11 +1,15 @@
 # horoscope_app/urls.py
 from django.urls import path
+from django.conf import settings
+from django.views.static import serve
+import os
 from . import views
 from .views import horoscope_detail
 
 app_name = 'horoscope_app'
 
 urlpatterns = [
+    path('ads.txt', serve, {'path': 'ads.txt', 'document_root': os.path.join(settings.BASE_DIR, '..', 'aihoroscope')}),
     path('', views.index, name='index'),
     path('horoscope/', views.horoscope, name='horoscope'),  # GET用のホロスコープAPI
     path('analyze/', views.analyze, name='analyze'),         # POSTで解析→OpenAI
